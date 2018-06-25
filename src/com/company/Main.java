a4e41c74b669eebf6e7f5d9951e0a77cd15805a7
niom21 <arturezg@gmail.com>
2018-06-25 17:18:06 +0200

    Change path to db

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,19 +1,20 @@
 package com.company;
 
 import com.company.service.FileReaderService;
+import com.company.service.FisherMethod;
 
 import java.io.IOException;
 
 public class Main {
 
     public static void main(String[] args) {
-
         FileReaderService fileReaderService = new FileReaderService();
+        FisherMethod fisherMethod = new FisherMethod();
         try {
             fileReaderService.readFile();
         } catch (IOException e) {
             e.printStackTrace();
         }
-//        fisherMethod.testFisher(fileReaderService.getRecordDataArrayList().get(1).getFeatureMatrix(), fileReaderService.getRecordDataArrayList().get(44).getFeatureMatrix());
+        fisherMethod.testFisher(fileReaderService.getRecordDataArrayList().get(1).getFeatureMatrix(), fileReaderService.getRecordDataArrayList().get(44).getFeatureMatrix());
     }
 }

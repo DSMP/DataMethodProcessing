bb0bdf824bec1defe889c3c9ecb8dbe325b5cdb6
damian <damiakuz@wp.pl>
2018-07-28 15:55:52 +0200

    ADD: Adding Adapter for NM method

---
+++ src/main/java/com/company/Adapters/AdapterNM.java
@@ -0,0 +1,52 @@
+package com.company.Adapters;
+
+import com.company.model.DataToTask3Model;
+import com.company.model.FeatureModel;
+
+import java.util.ArrayList;
+
+public class AdapterNM {
+    ArrayList<FeatureModel> allData;
+    ArrayList<FeatureModel> arrayListTraining;
+    ArrayList<DataToTask3Model> arrayListTest;
+    boolean isOutputPrepared = false;
+
+    public AdapterNM(ArrayList<FeatureModel> allData) {
+        arrayListTraining = new ArrayList<>();
+        arrayListTest = new ArrayList<>();
+        this.allData = allData;
+    }
+
+    public ArrayList<FeatureModel> getTrainingList()
+    {
+        preparedOutput();
+        return arrayListTraining;
+    }
+
+    public ArrayList<DataToTask3Model> getTestList() {
+        preparedOutput();
+        return arrayListTest;
+    }
+
+    private void preparedOutput()
+    {
+        if (isOutputPrepared) return;
+        for (FeatureModel featureModel: allData) {
+            int index = 0;
+            double[][] featureDataOut = new double[featureModel.getFeatureMatrix().length-(featureModel.getFeatureMatrix().length/5)][64];
+            for (int i = 0; i < featureModel.getFeatureMatrix().length; i++) {
+                if ((i % 5) == 0)
+                    arrayListTest.add(new DataToTask3Model(featureModel.getFeatureName(),featureModel.getFeatureMatrix()[i]));
+                else
+                {
+                    for (int j = 0; j < 64; j++) {
+                        featureDataOut[index][j] = featureModel.getFeatureMatrix()[i][j];
+                    }
+                    index++;
+                }
+            }
+            arrayListTraining.add(new FeatureModel(featureModel.getFeatureName(),featureDataOut));
+        }
+        isOutputPrepared = true;
+    }
+}
